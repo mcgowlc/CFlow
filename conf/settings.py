82090d4505db177a13162cab8c3607ca7e881086
Linda <mcgowlc@live.com>
2019-10-25 10:56:41 -0400

    heroku push

--- conf/settings.py
+++ conf/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts.apps.AccountsConfig',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend/static/build/static'),
+)
+
 AUTH_USER_MODEL = 'accounts.User'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ frontend/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+
+from .views import IndexView
+
+app_name = 'frontend'
+
+urlpatterns = [
+path('', IndexView.as_view(), name='index'),
+]
